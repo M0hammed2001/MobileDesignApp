98d4912feea14ea6774d644c3f7c0b0beb174096
Mohammed Sarhan <200150484@aston.ac.uk>
2023-10-23 17:51:08 +0100

    setup api

---
+++ app/src/main/java/uk/ac/aston/cs3mdd/mobiledesignproject/ui/Train/TrainAPI/TrainList.java
@@ -0,0 +1,18 @@
+package uk.ac.aston.cs3mdd.mobiledesignproject.ui.Train.TrainAPI;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class TrainList {
+
+    private ArrayList<TrainService> trainServices;
+
+    public String size() {
+        return size();
+    }
+
+
+    public ArrayList<TrainService> getResults() {
+        return trainServices;
+    }
+}
